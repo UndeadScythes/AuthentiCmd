82cd1310a43c863e31af817b461ab9a127c3d1cb
UndeadScythes <daveyognaut2@gmail.com>
2013-09-23 17:14:27 +0100

    Force implementation to use TipRedirect

--- src/test/java/authenticmdtest/implementation/TheProgram.java
+++ src/test/java/authenticmdtest/implementation/TheProgram.java
@@ -2,13 +2,12 @@ package authenticmdtest.implementation;
 
 import com.undeadscythes.authenticmd.*;
 import com.undeadscythes.tipscript.*;
-import java.util.logging.*;
 
 /**
  * @author UndeadScythes
  */
 public class TheProgram extends AuthentiCmd {
     public TheProgram() {
-        super(System.in, new TipScript(Logger.getLogger("test"), ""));
+        super(System.in, new TipRedirect());
     }
 }
